3fcd5c877699473e1a40edd14febf68e43fa3043
ullasmo <ullasa.me18@bmsce.ac.in>
2023-02-09 11:52:51 +0530

    Authorization changes

--- server/server.js
+++ server/server.js
@@ -35,6 +35,9 @@ app.post("/", async (req, res) => {
       frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
       presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
     });
+    const headers = {
+      Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+    };
 
     res.status(200).send({
       bot: response.data.choices[0].text,
